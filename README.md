cf6f391ead20af70f728e7e9793f4c6774b9de3f
James Ivings <james.ivings@gmail.com>
2017-04-01 17:47:57 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# version.js
+
+`version.js` allows you to display the latest version number of your webapp, shiny! ✨
+
+## Enable API access
+
+First you need to grab your ReleasePage API key. Learn more about the ReleasePage API [here][1].
+
+## Installation
+
+Include the library from our CDN, providing your API key and the ReleasePage ID.
+
+```html
+<script src="//cdn.releasepage.co/js/version.js" data-api-key="<API_KEY>" data-page-id="<PAGE_ID>" ></script>
+```
+
+## Badges
+
+### Version
+
+Any elements with the attribute `data-version-badge` will be populated with the latest version number on page load.
+
+```html
+<p>The latest version is <span data-version-badge></span></p>
+```
+
+### Name
+Any elements with the attribute `data-repo-name` will display the friendly name of the repository.
+
+```html
+<p>The latest version of <span data-repo-name></span> is <span data-version-badge></span></p>
+```
+
+## Grouped versions
+
+If your ReleasePage has [grouped versions][2] then you can display the repo versions individually, or as a grouped version.
+
+```html
+<p data-version-for="releasepage/version.js">
+  The latest version of <span data-repo-name></span> is <span data-version-badge></span>
+</p>
+```
+
+Without providing a `data-version-for` container, the badges will always display the grouped version if available.
+
+## Other version stuff 🚀
+
+Badges are awesome, but you can actually do a lot more with the version information we send you. `version.js` will expose a `Version` object on the `window` for you to play havoc with the version info.
+
+```node
+// the tag number of the release
+Version.tag()
+// "v1.0.1"
+
+// who published the release
+Version.author()
+// "Jivings"
+
+// when the release was published
+Version.publishedAt()
+// "2017-03-26T15:28:42Z"
+```
+
+With grouped versions this information will be a little more interesting.
+
+```node
+// the latest grouped version
+Version.tag()
+// "v0.4.0"
+
+// all the authors that contributed to this group of releases
+Version.author()
+// [ "Jivings", "dinkydani" ]
+
+// ok so this is the same (when the latest release was published)
+Version.publishedAt()
+// "2017-03-26T15:28:42Z"
+
+```
+
+If there is more than one repo in your ReleasePage, then the above functions will return the details for the repo with the most recent release. If you pass the name of the repo then you can override this;
+
+```node
+Version.tag({ repo: 'releasepage/version.js' })
+// v1.0.2
+```
+
+
+[1]: https://help.releasepage.co/api/getting-started
+[2]: https://help.releasepage.co/
---
+++ version.js
@@ -0,0 +1,131 @@
+const BASE_URL = window.__version_base_url || 'http://staging.api.releasepage.co';
+const MANAGE_URL = window.__manage_base_url || 'https://staging.manage.releasepage.co';
+
+const Version = function (opts) {
+  this.options = opts;
+};
+
+Version.prototype = {
+  load() {
+    const onLoad = this.onLoad.bind(this);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function () {
+      onLoad({ status: this.status, response: this.response });
+    });
+    const url = `${BASE_URL}/v1/pages/${this.options.page}/version?apiKey=${this.options.apiKey}`;
+    xhr.open('GET', url);
+    xhr.send();
+  },
+
+  onLoad({ status, response }) {
+    switch (status) {
+      case 404:
+        return console.error(`version.js: A page by name ${this.options.page}`);
+      case 401: {
+        const apiUrl = `${MANAGE_URL}/${this.options.page}/add-ons/api`;
+        console.error(`version.js: enable the version api for this page: ${apiUrl}`);
+        break;
+      }
+      case 200: {
+        const data = JSON.parse(response);
+        this.latest = data.latest;
+        this.latestGrouped = data.latestGrouped;
+        this.permalink = data.permalink;
+        break;
+      }
+      default:
+        return console.error('version.js: ReleasePage API error', status);
+    }
+
+    const badgeEls = document.querySelectorAll('[data-version-badge]');
+    if (badgeEls.length) {
+      this.renderBadges({ elements: badgeEls });
+    }
+    const repoNameEls = document.querySelectorAll('[data-repo-name]');
+    if (repoNameEls.length) {
+      this.renderRepoNames({ elements: repoNameEls });
+    }
+    return this;
+  },
+
+  renderRepoNames({ elements }) {
+    return elements.forEach((el) => {
+      const repoId = el.getAttribute('data-repo-name');
+      const repoDetails = this.latest.find(l => l.repo === repoId);
+      if (!repoDetails) return;
+      // eslint-disable-next-line no-param-reassign
+      el.textContent = repoDetails.name || repoDetails.repo;
+    });
+  },
+
+  renderBadges({ elements }) {
+    elements.forEach((el) => {
+      if (el.tagName === 'A' && !el.href) {
+        el.setAttribute('href', this.permalink);
+      }
+      const repo = el.getAttribute('data-version-repo');
+      let version;
+      if (repo) {
+        const repoVersion = this.latest.find(l => l.repo === repo);
+        version = repoVersion.version;
+      }
+      if (!version && this.latestGrouped) {
+        version = this.latestGrouped.version;
+      }
+      // eslint-disable-next-line no-param-reassign
+      el.textContent = version;
+    });
+  },
+
+  publishedAt({ repo } = {}) {
+    if (this.isGrouped()) {
+      return this.latestGrouped.published_at;
+    }
+    if (repo) {
+      const repoDetails = this.latest.find(l => l.repo === repo);
+      return repoDetails ? repoDetails.published_at : null;
+    }
+    return this.latest[0].published_at;
+  },
+
+  author({ repo } = {}) {
+    if (this.isGrouped()) {
+      return this.latestGrouped.authors;
+    }
+    if (repo) {
+      const repoDetails = this.latest.find(l => l.repo === repo);
+      return repoDetails ? repoDetails.author : null;
+    }
+    return this.latest[0].author;
+  },
+
+  tag({ repo } = {}) {
+    if (this.isGrouped()) {
+      return this.latestGrouped.version;
+    }
+    if (repo) {
+      const repoDetails = this.latest.find(l => l.repo === repo);
+      return repoDetails ? repoDetails.version : null;
+    }
+    return this.latest[0].version;
+  },
+
+  isGrouped() {
+    return !!this.latestGrouped;
+  }
+};
+
+if (typeof window !== 'undefined') {
+  // set up automatically
+  document.addEventListener('DOMContentLoaded', () => {
+    const el = document.querySelector('script[data-page-id]');
+    if (!el) return console.error('version.js: no key provided');
+    const page = el.getAttribute('data-page-id');
+    const apiKey = el.getAttribute('data-api-key');
+    window.Version = new Version({
+      page,
+      apiKey
+    });
+    return window.Version.load();
+  });
+}
